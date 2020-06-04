a4b1b873928eaf3e652cdcfef69e72edc51cf77b
v.shapkarin <v.shapkarin@corp.rtln.ru>
2020-06-04 19:04:12 +0300

    HW07 optional

--- web/webapp/src/main/java/ru/javaops/masterjava/webapp/UsersServlet.java
+++ web/webapp/src/main/java/ru/javaops/masterjava/webapp/UsersServlet.java
@@ -4,6 +4,8 @@ import com.google.common.collect.ImmutableMap;
 import org.thymeleaf.context.WebContext;
 import ru.javaops.masterjava.persist.DBIProvider;
 import ru.javaops.masterjava.persist.dao.UserDao;
+import ru.javaops.masterjava.service.mail.Addressee;
+import ru.javaops.masterjava.service.mail.MailWSClient;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -11,6 +13,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import static ru.javaops.masterjava.common.web.ThymeleafListener.engine;
 
@@ -23,5 +29,22 @@ public class UsersServlet extends HttpServlet {
         final WebContext webContext = new WebContext(req, resp, req.getServletContext(), req.getLocale(),
                 ImmutableMap.of("users", userDao.getWithLimit(20)));
         engine.process("users", webContext, resp.getWriter());
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String[] to = req.getParameterValues("to");
+        String[] cc = req.getParameterValues("cc");
+        String subject = req.getParameter("subject");
+        String body = req.getParameter("body");
+
+        Set<Addressee> toList = Arrays.stream(to)
+                .map(Addressee::new)
+                .collect(Collectors.toSet());
+        Set<Addressee> ccList = Arrays.stream(cc)
+                .map(Addressee::new)
+                .collect(Collectors.toSet());
+
+        MailWSClient.sendToGroup(toList, ccList, subject, body);
     }
 }
